d9752f53b7c005ff03cba6136da4a8d5f04a4ece
Dustin L. Dodson <dustinleedodson@gmail.com>
2014-10-30 11:55:17 -0800

    Added more functions to vhdl class -- can now generate basic VHDL code.

--- VHDL Generation.py
+++ VHDL Generation.py
@@ -6,42 +6,77 @@ alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r'
 class vhdl:
 	"""A helper class for generating VHDL code!"""
 	def __init__(self):
-		self.name = raw_input("Please enter the name of your module: ")
+		self.entityName = raw_input("Please enter the name of your entity: ")
+		self.architectureName = raw_input("Please enter the name of your architecture: ")
 		self.numInputs = input("Number of input variables: ")
 		self.numOutputs = input("Number of output variables: ")
-		self.numSignals = input("Number of signal variables: ")
+		self.numEntitySignals = input("Number of signal variables in entity: ")
+		self.numArchitectureSignals = input("Number of signal variables in architecture: ")
+		self.inputNames, self.outputNames = [], []
 		
-	def getInputNames():
-		self.inputNames = []
+		choice = raw_input("Do you want to name your variables?")
+		if choice.lower() == 'y':
+			self.getInputNames()
+			self.getOutputNames()
+		
+	def getInputNames(self):
 		for i in range(self.numInputs):
 			self.inputNames.append(raw_input("Please enter the variable name for input #%s: " % (i+1)))
 		
-	def getOutputNames():
-		self.outputNames = []
+	def getOutputNames(self):
 		for i in range(self.numOutputs):
 			self.outputNames.append(raw_input("Please enter the variable name for output #%s: " % (i+1)))
 		
-	def generateEntity():
-		entityDef = "ENTITY %s IS\n	PORT(" % self.name
+	def generateEntity(self):
+		self.entity = "ENTITY %s IS\n	PORT(" % self.entityName
 		
 		if self.numInputs == len(self.inputNames):
 			for i in range(self.numInputs):
-				entityDef += self.inputNames[i]
+				self.entity += self.inputNames[i]
 				if i != (self.numInputs-1):
-					entityDef += ', '
+					self.entity += ', '
+		else:
+			for i in range(self.numInputs):
+				self.entity += "x%s" % i
+				if i != (self.numInputs-1):
+					self.entity += ', '
 		
-		entityDef += "	:IN STD_LOGIC;\n		"
+		self.entity += "	:IN STD_LOGIC;\n		"
 		
 		if self.numOutputs == len(self.outputNames):
 			for i in range(self.numOutputs):
-				entityDef += self.outoutNames[i]
+				self.entity += self.outputNames[i]
 		
 				if(i != (self.numOutputs-1)):
-					entityDef += ", "
+					self.entity += ", "
+		else:
+			for i in range(self.numOutputs):
+				self.entity += alpha[i]
+				if i != (self.numOutputs-1):
+					self.entity += ', '
 		
-		entityDef += "	:OUT STD_LOGIC);\nEND ENTITY %s;" % self.name
+		self.entity += "	:OUT STD_LOGIC);\nEND ENTITY %s;" % self.entityName
+		
+	def generateArchitecture(self):
+		self.architecture = "ARCHITECTURE %s OF %s IS\nBEGIN\n" % (self.architectureName, self.entityName)
+		
+		if self.numOutputs == len(self.outputNames):
+			for i in range(self.numOutputs):
+				self.architecture += "	%s <= --define variable here!\n" % self.outputNames[i]
+		else:
+			for i in range(self.numOutputs):
+				self.architecture += "	%s <= --define variable here!\n" % alpha[i]
+		
+		self.architecture += "END LogicFunc;"
 	
-		return entityDef
+	def printVHDL(self):
+		self.generateEntity()
+		self.generateArchitecture()
+		print "LIBRARY ieee;\nUSE ieee.std_logic_1164.all;"
+		print ""
+		print self.entity
+		print ""
+		print self.architecture
 
 def getInfo():
 	module = raw_input("Please enter the name of your module: ")
@@ -53,31 +88,36 @@ def getInfo():
 #Working!
 def generateEntity(info):
 	moduleName, inputs, outputs = info
-	
+
 	entityDef = "ENTITY %s IS\n	PORT(" % moduleName
-	
+
 	for i in range(inputs):
 		entityDef += "x%s" % (i+1)
 		if(i != (inputs-1)):
 			entityDef += ", "
-	
+
 	entityDef += "	:IN STD_LOGIC;\n		"
-	
+
 	for i in range(outputs):
 		entityDef += alpha[i]
-		
+	
 		if(i != (outputs-1)):
 			entityDef += ", "
-		
-	entityDef += "	:OUT STD_LOGIC);\nEND ENTITY %s;" % moduleName
 	
-	return entityDef	
+	entityDef += "	:OUT STD_LOGIC);\nEND ENTITY %s;" % moduleName
+
+	return entityDef
 
 def generateArchitecture(module):
 	return ("ARCHITECTURE LogicFunc OF %s IS\nBEGIN\n	-- Insert function definition here\nEND LogicFunc;" % module)
 
+"""
 moduleName, inputs, outputs = getInfo()
 print "LIBRARY ieee;\nUSE ieee.std_logic_1164.all;"
 print generateEntity((moduleName, inputs, outputs))
 print ""
 print generateArchitecture(moduleName)
+"""
+
+myVHDL = vhdl()
+myVHDL.printVHDL()
