3f966995e2474c4b3490fdccc3bc383df82727e8
Dustin L. Dodson <dustinleedodson@gmail.com>
2014-10-30 12:14:17 -0800

    vhdl class can now generate the package code.

--- VHDL Generation.py
+++ VHDL Generation.py
@@ -14,7 +14,7 @@ class vhdl:
 		self.numArchitectureSignals = input("Number of signal variables in architecture: ")
 		self.inputNames, self.outputNames = [], []
 		
-		choice = raw_input("Do you want to name your variables?")
+		choice = raw_input("Do you want to name your variables? [Y/N]: ")
 		if choice.lower() == 'y':
 			self.getInputNames()
 			self.getOutputNames()
@@ -69,6 +69,39 @@ class vhdl:
 		
 		self.architecture += "END LogicFunc;"
 	
+	def generatePackage(self):
+		self.packageName = raw_input("Please enter a name for the package: ")
+		
+		self.package = "LIBRARY ieee;\nUSE ieee.std_logic_1164.all;"
+		self.package += "PACKAGE %s IS\n" % self.packageName
+		self.package += "	COMPONENT %s\n" % self.entityName
+		self.package += "		PORT("
+		
+		for i in range(self.numInputs):
+			if len(self.inputNames) > 0:
+				self.package += inputNames[i]
+			else:
+				self.package += "x%s" % i
+			
+			if i != (self.numInputs - 1):
+				self.package += ', '
+		
+		self.package += "	: IN STD_LOGIC;\n"
+		self.package += "			"
+		
+		for i in range(self.numOutputs):
+			if len(self.outputNames) > 0:
+				self.package += outputNames[i]
+			else:
+				self.package += alpha[i]
+			
+			if i != (self.numOutputs - 1):
+				self.package += ', '
+		
+		self.package += "	: OUT STD_LOGIC);\n"
+		self.package += "	END COMPONENT;\n"
+		self.package += "END %s;" % self.packageName
+	
 	def printVHDL(self):
 		self.generateEntity()
 		self.generateArchitecture()
@@ -77,6 +110,12 @@ class vhdl:
 		print self.entity
 		print ""
 		print self.architecture
+		
+		choice = raw_input("Do you want to package the entity? [Y/N]: ")
+		if choice.lower() == 'y':
+			self.generatePackage()
+			print ""
+			print self.package
 
 myVHDL = vhdl()
 myVHDL.printVHDL()
