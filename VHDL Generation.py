0ca3267be5db380bcd8e5681a3a684701b86dcfb
Dustin L. Dodson <dustinleedodson@gmail.com>
2014-10-30 11:56:37 -0800

    Got rid of older functions, as they are no longer necessary.

--- VHDL Generation.py
+++ VHDL Generation.py
@@ -78,46 +78,5 @@ class vhdl:
 		print ""
 		print self.architecture
 
-def getInfo():
-	module = raw_input("Please enter the name of your module: ")
-	numInputs = input("Number of input variables: ")
-	numOutputs = input("Number of output variables: ")
-	
-	return (module, numInputs, numOutputs)
-
-#Working!
-def generateEntity(info):
-	moduleName, inputs, outputs = info
-
-	entityDef = "ENTITY %s IS\n	PORT(" % moduleName
-
-	for i in range(inputs):
-		entityDef += "x%s" % (i+1)
-		if(i != (inputs-1)):
-			entityDef += ", "
-
-	entityDef += "	:IN STD_LOGIC;\n		"
-
-	for i in range(outputs):
-		entityDef += alpha[i]
-	
-		if(i != (outputs-1)):
-			entityDef += ", "
-	
-	entityDef += "	:OUT STD_LOGIC);\nEND ENTITY %s;" % moduleName
-
-	return entityDef
-
-def generateArchitecture(module):
-	return ("ARCHITECTURE LogicFunc OF %s IS\nBEGIN\n	-- Insert function definition here\nEND LogicFunc;" % module)
-
-"""
-moduleName, inputs, outputs = getInfo()
-print "LIBRARY ieee;\nUSE ieee.std_logic_1164.all;"
-print generateEntity((moduleName, inputs, outputs))
-print ""
-print generateArchitecture(moduleName)
-"""
-
 myVHDL = vhdl()
 myVHDL.printVHDL()
